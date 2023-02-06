630413d08f568873004609122eefbda55a36301a
Veaceslav Radut <radut01@gmail.com>
2023-02-06 16:46:39 +0200

    Initial commit

---
+++ Payment/src/main/java/com/amdaris/mantoring/payment/PaymentService.java
@@ -0,0 +1,11 @@
+package com.amdaris.mantoring.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentService {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentService.class);
+    }
+}
