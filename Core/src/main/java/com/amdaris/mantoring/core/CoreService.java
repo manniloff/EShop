49541ac0df3ff54bd497dda105b51f961230f7fd
Veaceslav Radut <radut01@gmail.com>
2023-02-06 16:46:39 +0200

    Initial commit

---
+++ Core/src/main/java/com/amdaris/mantoring/core/CoreService.java
@@ -0,0 +1,11 @@
+package com.amdaris.mantoring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreService {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreService.class);
+    }
+}
---
+++ Shipment/src/main/java/com/amdaris/mantoring/shipment/ShipmentService.java
@@ -0,0 +1,11 @@
+package com.amdaris.mantoring.shipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentService {
+    public static void main(String[] args) {
+        SpringApplication.run(ShipmentService.class);
+    }
+}
